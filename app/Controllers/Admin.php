38cdb1126b35d5a913bcf6526cbcb15d4ec9aa95
tiago <tiagolemespalhano@gmail.com>
2022-08-28 20:55:16 -0300

    Iniciando admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Admin extends BaseController
 {
     public function index()
     {
-        return view('admin/index');
+        return view('admin/pages/index');
     }
 
 }
